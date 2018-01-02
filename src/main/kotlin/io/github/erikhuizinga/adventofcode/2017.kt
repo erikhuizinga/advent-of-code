90f581ed5e694e94bbeefaed67d2bf50fc4f8a0b
Erik Huizinga <erik.huizinga@nedap.com>
2018-01-02 15:26:39 +0100

    Resolve day 7 part 1 and solve day 7 part 2

--- src/main/kotlin/io/github/erikhuizinga/adventofcode/2017.kt
+++ src/main/kotlin/io/github/erikhuizinga/adventofcode/2017.kt
@@ -4,9 +4,7 @@ import java.net.URL
 import kotlin.math.*
 
 /** Created by erik.huizinga on 27-12-17 */
-fun main(args: Array<String>) {
-  day7Part1()
-}
+fun main(args: Array<String>) = day7Part2()
 
 val captcha1: List<String> = listOf("1122", "1111", "1234", "91212129", "823936645345581272695677318513459491834641129844393742672553544439126314399846773234845535593355348931499496184839582118817689171948635864427852215325421433717458975771369522138766248225963242168658975326354785415252974294317138511141826226866364555761117178764543435899886711426319675443679829181257496966219435831621565519667989898725836639626681645821714861443141893427672384716732765884844772433374798185955741311116365899659833634237938878181367317218635539667357364295754744829595842962773524584225427969467467611641591834876769829719248136613147351298534885563144114336211961674392912181735773851634298227454157885241769156811787611897349965331474217223461176896643242975397227859696554492996937235423272549348349528559432214521551656971136859972232854126262349381254424597348874447736545722261957871275935756764184378994167427983811716675476257858556464755677478725146588747147857375293675711575747132471727933773512571368467386151966568598964631331428869762151853634362356935751298121849281442128796517663482391226174256395515166361514442624944181255952124524815268864131969151433888721213595267927325759562132732586252438456569556992685896517565257787464673718221817783929691626876446423134331749327322367571432532857235214364221471769481667118117729326429556357572421333798517168997863151927281418238491791975399357393494751913155219862399959646993428921878798119215675548847845477994836744929918954159722827194721564121532315459611433157384994543332773796862165243183378464731546787498174844781781139571984272235872866886275879944921329959736315296733981313643956576956851762149275521949177991988236529475373595217665112434727744235789852852765675189342753695377219374791548554786671473733124951946779531847479755363363288448281622183736545494372344785112312749694167483996738384351293899149136857728545977442763489799693492319549773328626918874718387697878235744154491677922317518952687439655962477734559232755624943644966227973617788182213621899579391324399386146423427262874437992579573858589183571854577861459758534348533553925167947139351819511798829977371215856637215221838924612644785498936263849489519896548811254628976642391428413984281758771868781714266261781359762798")
 
@@ -312,29 +310,79 @@ val testInputDay7 =
         "gyxo (61)\n" +
         "cntj (57)"
 
-fun testDay7Part1() = println(testInputDay7.parseTower().bottom)
-
-fun String.parseTower(): Tower =
-    split('\n')
-        .filter(String::isNotEmpty)
-        .map { "\\w+|\\d+".toRegex().findAll(it) }
-        .map { it.toList().map(MatchResult::value) }
-        .map { Program(it[0], it[1].toInt(), it.drop(2)) }
-        .apply {
-          forEach { program -> program.subPrograms = filter { it.name in program.subProgramNames } }
-        }
-
-typealias Tower = Collection<Program>
-
-data class Program(
-    val name: String,
-    val weight: Int,
-    val subProgramNames: Collection<String>,
-    var subPrograms: Tower = emptySet()
-)
-
-val Tower.bottom get() = single { it.name !in flatMap(Program::subProgramNames) }
-
 val inputDay7 = FileProvider.url("/inputDay7.txt")!!.readText()
 
-fun day7Part1() = println(inputDay7.parseTower().bottom)
+fun testDay7Part1() = println(findBottomProgramName(testInputDay7))
+
+fun day7Part1() = println(findBottomProgramName(inputDay7))
+
+val nameRegex = "[A-z]+".toRegex()
+
+fun findBottomProgramName(input: String): String =
+    input
+        .run { nameRegex.findAll(this) }
+        .map(MatchResult::value)
+        .run { single { programName -> programName !in minus(programName) } }
+
+fun testDay7Part2() = println(findTargetWeight(testInputDay7))
+
+fun day7Part2() = println(findTargetWeight(inputDay7))
+
+fun findTargetWeight(input: String): Int {
+  val bottomProgramName = findBottomProgramName(input)
+  val weightGroups = generateSequence(
+      Program.parse(input).single { it.name == bottomProgramName }.subPrograms
+  ) {
+    it
+        .groupBy(Program::totalWeight)
+        .values
+        .singleOrNull {
+          it.size == 1
+              && it.first().subPrograms.map { it.totalWeight }.distinct().size > 1
+        }
+        ?.first()
+        ?.subPrograms
+  }
+      .last()
+      .groupBy(Program::totalWeight)
+  val unbalancedProgram = weightGroups.values.single { it.size == 1 }.first()
+  return (weightGroups.keys - unbalancedProgram.totalWeight)
+      .single() - unbalancedProgram.totalWeight + unbalancedProgram.weight
+}
+
+class Program(
+    val name: String,
+    val weight: Int,
+    val subPrograms: MutableSet<Program> = mutableSetOf()
+) {
+
+  val totalWeight: Int
+    get() = weight + subPrograms.sumBy(Program::totalWeight)
+
+  companion object {
+    fun parse(input: String): Set<Program> {
+      val programLines = input.split('\n').filterNot(String::isEmpty)
+      return programLines
+          .mapNotNull {
+            Program(
+                nameRegex.find(it)?.value ?: return@mapNotNull null,
+                "\\d+".toRegex().find(it)?.value?.toInt() ?: return@mapNotNull null
+            )
+          }
+          .toSet() // Set of programs with subprograms not yet added
+          .apply {
+            // Find and add all subprograms
+            programLines.forEach { programLine ->
+              nameRegex
+                  .findAll(programLine)
+                  .map(MatchResult::value)
+                  .let { names ->
+                    single { program -> program.name == names.first() }
+                        .subPrograms
+                        .addAll(filter { it.name in names.drop(1) })
+                  }
+            }
+          }
+    }
+  }
+}
